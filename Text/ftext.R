4ef90eb21ae643a62ec3698d6035c6e589574d6e
Romina Elena Mendez <39013829+r0mymendez@users.noreply.github.com>
2020-02-02 13:03:17 -0300

    Delete ftext.R

--- Text/ftext.R
+++
@@ -1,9 +0,0 @@
-
-SacarAcentos=function(palabra){
-  palabra=gsub('á','a',palabra)
-  palabra=gsub('é','e',palabra)
-  palabra=gsub('í','i',palabra)
-  palabra=gsub('ó','o',palabra)
-  palabra=gsub('ú','u',palabra)
-  return(palabra)
-}
