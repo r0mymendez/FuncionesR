a8773b1a58b01d62ec3515c80faf54dcba5c0f02
Romina Elena Mendez <39013829+r0mymendez@users.noreply.github.com>
2020-02-02 12:53:47 -0300

    Add files via upload

---
+++ Text/ftext.R
@@ -0,0 +1,8 @@
+
+SacarAcentos=function(palabra){
+  palabra=gsub('á','a',palabra)
+  palabra=gsub('é','e',palabra)
+  palabra=gsub('í','i',palabra)
+  palabra=gsub('ó','o',palabra)
+  palabra=gsub('ú','u',palabra)
+}
