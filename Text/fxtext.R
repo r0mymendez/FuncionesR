ff068b75484b72f54cea1bc9bf1763324d46b68b
Romina Elena Mendez <39013829+r0mymendez@users.noreply.github.com>
2020-02-02 13:09:14 -0300

    Add files via upload

--- Text/fxtext.R
+++ Text/fxtext.R
@@ -7,3 +7,11 @@ SacarAcentos=function(palabra){
   palabra=gsub('ú','u',palabra)
   return(palabra)
 }
+
+SacarStopWord=function(columna){
+  sw=read.csv('https://raw.githubusercontent.com/stopwords-iso/stopwords-es/master/stopwords-es.txt',
+              header = F,encoding = 'UTF-8')
+  
+  resultado=columna[!(columna %in% sw$V1),]
+  return(resultado)
+}
